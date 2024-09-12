486b3b7227976fef0f13af89dabd639f12199aba
Jayabrata18 <jayabrata180402@gmail.com>
2024-09-13 02:56:36 +0530

    Add Toaster component and import it in layout.tsx

--- app/dashboard/blog/create/page.tsx
+++ app/dashboard/blog/create/page.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+"use client";
 
-const page = () => {
-    return <div>blog editing page</div>;
-};
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/hooks/use-toast";
+import { EyeOpenIcon } from "@radix-ui/react-icons";
+const FormSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters."
+    })
+});
 
-export default page;
+export default function BlogForm() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            username: ""
+        }
+    });
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        toast({
+            title: "You submitted the following values:",
+            description: (
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+                </pre>
+            )
+        });
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full border rounded-md space-y-6">
+                <div className="p-5">
+                    <span role="button" tabIndex={0} className="flex items-cennter gap-1 border bg-zine-700">
+                        <EyeOpenIcon />
+                    </span>
+                </div>
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>This is your public display name.</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Navbar from "@/components/nav/Navbar";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
                       <Navbar />
                       {children}
                   </main>
+                  <Toaster />
               </ThemeProvider>
           </body>
       </html>
