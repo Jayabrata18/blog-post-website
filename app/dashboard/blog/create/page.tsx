cd9b01a15a0b8b3680183a2e98d8b6d5d0ecfcd0
Jayabrata18 <jayabrata180402@gmail.com>
2024-09-13 23:55:29 +0530

    Add MarkdownPreview component and Textarea component; update next.config.mjs for image remote patterns

--- app/dashboard/blog/create/page.tsx
+++ app/dashboard/blog/create/page.tsx
@@ -4,21 +4,44 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/hooks/use-toast";
-import { EyeOpenIcon } from "@radix-ui/react-icons";
+import { EyeOpenIcon, Pencil1Icon } from "@radix-ui/react-icons";
+import { Switch } from "@/components/ui/switch";
+import { RocketIcon, StarIcon } from "lucide-react";
+import { BsSave } from "react-icons/bs";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Textarea } from "@/components/ui/textarea";
+import MarkdownPreview from "@/components/markdown/MarkdownPreview";
+
 const FormSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters."
-    })
+    title: z.string().min(2, {
+        message: "Title must be at least 2 characters."
+    }),
+    image_url: z.string().url({
+        message: "Invalid URL"
+    }),
+    content: z.string().min(10, {
+        message: "Content must be at least 10 characters."
+    }),
+    isPremium: z.boolean(),
+    isPublished: z.boolean()
 });
 
 export default function BlogForm() {
+    const [isPreview, setPreview] = useState(false);
     const form = useForm<z.infer<typeof FormSchema>>({
+        mode: "all",
         resolver: zodResolver(FormSchema),
         defaultValues: {
-            username: ""
+            title: "",
+            content: "",
+            image_url: "",
+            isPremium: false,
+            isPublished: false
         }
     });
 
@@ -35,27 +58,154 @@ export default function BlogForm() {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full border rounded-md space-y-6">
-                <div className="p-5">
-                    <span role="button" tabIndex={0} className="flex items-cennter gap-1 border bg-zine-700">
-                        <EyeOpenIcon />
-                    </span>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full border rounded-md space-y-6 ">
+                <div className="p-5 flex items-center flex-wrap justify-between border-b gap-5">
+                    <div className="flex gap-5 items-center flex-wrap">
+                        <span
+                            role="button"
+                            tabIndex={0}
+                            className="flex items-center gap-1 border bg-zinc-700 p-2 rounded-md hover:ring-2 hover:ring-zinc-400 transition-all"
+                            onClick={() => setPreview(!isPreview)}
+                        >
+                            {isPreview ? (
+                                <>
+                                    <Pencil1Icon />
+                                    Edit
+                                </>
+                            ) : (
+                                <>
+                                    <EyeOpenIcon />
+                                    Preview
+                                </>
+                            )}
+                        </span>
+                        <FormField
+                            control={form.control}
+                            name="isPremium"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <div className="flex items-center gap-1 border bg-zinc-700 p-2 rounded-md hover:ring-2 hover:ring-zinc-400 transition-all">
+                                            <StarIcon />
+                                            <span>Premium</span>
+                                            <Switch checked={field.value} onCheckedChange={field.onChange} />
+                                        </div>
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="isPublished"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <div className="flex items-center gap-1 border bg-zinc-700 p-2 rounded-md hover:ring-2 hover:ring-zinc-400 transition-all">
+                                            <RocketIcon />
+                                            <span>Publish</span>
+                                            <Switch checked={field.value} onCheckedChange={field.onChange} />
+                                        </div>
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <Button className="flex items-center gap-1" disabled={!form.formState.isValid}>
+                        <BsSave />
+                        Save
+                    </Button>
                 </div>
                 <FormField
                     control={form.control}
-                    name="username"
+                    name="title"
                     render={({ field }) => (
                         <FormItem>
-                            <FormLabel>Username</FormLabel>
+                            <FormLabel></FormLabel>
                             <FormControl>
-                                <Input placeholder="shadcn" {...field} />
+                                <div className={cn("p-2 w-full flex break-words gap-2", isPreview ? "divide-x-0" : "divide-x")}>
+                                    <Input
+                                        placeholder="title"
+                                        {...field}
+                                        className={cn("border-none text-lg font-medium leading-relaxed", isPreview ? "w-0 p-0" : "w-full lg:w-1/2")}
+                                    />
+                                    <div className={cn("lg:px-10", isPreview ? "mx-auto w-full lg:w-4/5" : "w-1/2 lg:block hidden")}>
+                                        <h1 className="text-3xl font-medium">{form.getValues().title}</h1>
+                                    </div>
+                                </div>
                             </FormControl>
-                            <FormDescription>This is your public display name.</FormDescription>
+                            {form.getFieldState("title").invalid && form.getValues().title && <FormMessage />}
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="image_url"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel></FormLabel>
+                            <FormControl>
+                                <div className={cn("p-2 w-full flex break-words gap-2", isPreview ? "divide-x-0" : "divide-x")}>
+                                    <Input
+                                        placeholder="image url"
+                                        {...field}
+                                        className={cn("border-none text-lg font-medium leading-relaxed", isPreview ? "w-0 p-0" : "w-full lg:w-1/2")}
+                                    />
+                                    <div className={cn("lg:px-10", isPreview ? "mx-auto w-full lg:w-4/5" : "w-1/2 lg:block hidden")}>
+                                        {!isPreview ? (
+                                            <>
+                                                <p>Click on Preview to see</p>
+                                            </>
+                                        ) : (
+                                            <div className="relative h-80 mt-5 border rounded-md">
+                                                <Image
+                                                    src={form.getValues().image_url}
+                                                    alt="preview"
+                                                    fill
+                                                    className="object-cover object-center rounded-md"
+                                                />
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            </FormControl>
+                            {form.getFieldState("image_url").invalid && form.getValues().image_url && (
+                                <div className="p-2">
+                                    <FormMessage />
+                                </div>
+                            )}
+
                             <FormMessage />
                         </FormItem>
                     )}
                 />
-                <Button type="submit">Submit</Button>
+                <FormField
+                    control={form.control}
+                    name="content"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel></FormLabel>
+                            <FormControl>
+                                <div className={cn("p-2 w-full flex break-words gap-2", isPreview ? "divide-x-0 " : "divide-x h-70vh")}>
+                                    <Textarea
+                                        placeholder="content"
+                                        {...field}
+                                        className={cn(
+                                            "border-none text-lg font-medium leading-relaxed resize-none ",
+                                            isPreview ? "w-0 p-0" : "w-full lg:w-1/2"
+                                        )}
+                                    />
+                                    <div className={cn("lg:px-10", isPreview ? "mx-auto w-full lg:w-4/5" : "w-1/2 lg:block hidden")}>
+                                        <MarkdownPreview content={form.getValues().content} />
+                                    </div>
+                                </div>
+                            </FormControl>
+                            {form.getFieldState("content").invalid && form.getValues().content && <FormMessage />}
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
             </form>
         </Form>
     );
---
+++ components/markdown/MarkdownPreview.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MarkdownPreview = ({ content, className }: { content: string; className?: string }) => {
+    return <div>
+
+        {content}
+    </div>;
+};
+
+export default MarkdownPreview;
